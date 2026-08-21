1566bb00fd28205b4883b9c8f604ffca3c17557d
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-08-21 11:34:15 -0500

    add files

---
+++ base-main.js
@@ -0,0 +1,10 @@
+"use strict";
+//MagnificPopup Gallery
+
+$('.portfolio_icon').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery: {
+		enabled: true
+	},
+});
---
+++ slickslides.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$(document).ready(function(){
+	$('.word_slider').slick({
+		dots: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		vertical: true	
+	});
+	
+});
