f94f1c1ff26c389ec675f38478a243bf4acf0035
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-08-30 11:02:22 -0500

    add files

---
+++ agency-pro-main.js
@@ -0,0 +1,95 @@
+"use strict";
+//Slick Slider
+$(document).ready(function(){
+	$('.client-slider').slick({
+		dots: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '.prev_nav',
+		nextArrow: '.next_nav',
+		autoplay: false,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+	
+	});
+});
+
+//Animate Scroll
+$( ".about a" ).click(function() {
+	$('.info_box').animatescroll();
+});
+
+$( ".services a" ).click(function() {
+	$('.services_area').animatescroll();
+});
+
+$( ".strategies a" ).click(function() {
+	$('.strategies_area').animatescroll();
+});
+
+$( ".portfolio a" ).click(function() {
+	$('.portfolio_area').animatescroll();
+});
+
+$( ".blog a" ).click(function() {
+	$('.blog_area').animatescroll();
+});
+
+//MagnificPopup Gallery
+$('.portfolio_image').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery: {
+		enabled: true
+	},
+});
+
+//MagnificPopup Video
+$('#play-video').magnificPopup({
+	disableOn: 300,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 300,
+	preloader: false,
+	fixedContentPos: false
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
---
+++ startup-pro-main.js
@@ -0,0 +1,143 @@
+"use strict";
+//Slick Slider
+$(document).ready(function(){
+	$('.grid_slider').slick({
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		prevArrow: '.prev_slide',
+		nextArrow: '.next_slide',
+		autoplay: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		centerMode: true,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 2,
+					
+				}			
+			},
+			
+			{
+				breakpoint: 601,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: '100px',
+				}			
+			},
+			
+			{
+				breakpoint: 430,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: '35px',
+				}			
+			},
+			
+			{
+				breakpoint: 376,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: '20px',
+				}			
+			},
+		]
+	});
+	
+});
+
+//Slick Slider (3) Three
+$(document).ready(function(){
+	$('.client_slider').slick({
+		slidesToShow: 3,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: true,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 1,
+					fade: true
+				}			
+			},
+		]
+	});
+});
+
+//Animate Scroll
+$( ".about a" ).click(function() {
+	$('.about_area').animatescroll();
+});
+
+$( ".services a" ).click(function() {
+	$('.start-up_group_two').animatescroll();
+});
+
+$( ".products a" ).click(function() {
+	$('.start-up_group_three').animatescroll();
+});
+
+$( ".features a" ).click(function() {
+	$('.start-up_group_four').animatescroll();
+});
+
+$( ".blog a" ).click(function() {
+	$('.start-up_group_six').animatescroll();
+});
+
+//MagnificPopup Video
+$('#play-video').magnificPopup({
+	disableOn: 300,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 300,
+	preloader: false,
+	fixedContentPos: false
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
