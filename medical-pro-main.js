bc54d8e5fb82f0a1d9c1d8f4fb3c1b8cbdc1bb23
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-08-30 18:04:35 -0500

    add files

---
+++ medical-pro-main.js
@@ -0,0 +1,70 @@
+"use strict";
+//MagnificPopup Video
+$('#play-video').magnificPopup({
+	disableOn: 300,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 300,
+	preloader: false,
+	fixedContentPos: false
+});
+
+//Animate Scroll
+$( ".about a" ).click(function() {
+	$('.medical_area_two').animatescroll();
+});
+
+$( ".services a" ).click(function() {
+	$('.medical_area_one').animatescroll();
+});
+
+$( ".departments a" ).click(function() {
+	$('.medical_area_three').animatescroll();
+});
+
+$( ".practice a" ).click(function() {
+	$('.medical_area_four').animatescroll();
+});
+
+$( ".blog a" ).click(function() {
+	$('.medical_area_six').animatescroll();
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
