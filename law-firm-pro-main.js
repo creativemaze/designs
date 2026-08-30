f886535a7a58f1d519d7c047812d8726159ab054
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-08-30 15:01:32 -0500

    add files

---
+++ law-firm-pro-main.js
@@ -0,0 +1,121 @@
+"use strict";
+//Slick Slider (1) One
+$(document).ready(function(){
+	$('.border_slider').slick({
+		slidesToShow: 3,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 601,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+					fade: true
+				}			
+			},
+		]
+	});
+});
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.square_slider').slick({
+		prevArrow: '.prev_slide',
+		nextArrow: '.next_slide',
+		dots: false,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 2,
+       
+				}		
+			},
+			{
+				breakpoint: 601,
+				settings: {
+					slidesToShow: 1,
+       
+				}		
+			},
+		]
+		
+	});
+	
+});
+
+//Animate Scroll
+$( ".process a" ).click(function() {
+	$('.law_area_one').animatescroll();
+});
+
+$( ".services a" ).click(function() {
+	$('.law_area_two').animatescroll();
+});
+
+$( ".about a" ).click(function() {
+	$('.law_area_three').animatescroll();
+});
+
+$( ".cases a" ).click(function() {
+	$('.law_area_five').animatescroll();
+});
+
+$( ".attorneys a" ).click(function() {
+	$('.law_area_six').animatescroll();
+});
+
+$( ".blog a" ).click(function() {
+	$('.law_area_nine').animatescroll();
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
