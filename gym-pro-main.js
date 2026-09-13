1fbb336606b32999f69f75e849604fdb76f923f9
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-13 18:48:44 -0500

    add files

---
+++ gym-pro-main.js
@@ -0,0 +1,137 @@
+"use strict";
+//Slick Slider
+$(document).ready(function(){
+	$('.bg_slider').slick({
+		dots: false,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		centerMode: true,
+		responsive: [
+			{
+				breakpoint: 1025,
+				settings: {
+					slidesToShow: 1,
+					
+				}			
+			},
+			
+			{
+				breakpoint: 601,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: '50px',
+				}			
+			},
+			
+			{
+				breakpoint: 430,
+				settings: {
+					slidesToShow: 1,
+					centerPadding: '20px',
+				}			
+			},
+		]
+	});
+	
+});
+
+//Animate Scroll
+$( ".about a" ).click(function() {
+	$('.gym_area_two').animatescroll();
+});
+
+$( ".programs a" ).click(function() {
+	$('.gym_area_three').animatescroll();
+});
+
+$( ".benefits a" ).click(function() {
+	$('.gym_area_four').animatescroll();
+});
+
+$( ".schedule a" ).click(function() {
+	$('.gym_area_six').animatescroll();
+});
+
+$( ".blog a" ).click(function() {
+	$('.gym_area_seven').animatescroll();
+});
+
+// Add current class to the current button (highlight it)
+var btns = document.getElementsByClassName("accordion");
+for (var i = 0; i < btns.length; i++) {
+	btns[i].addEventListener("click", function(){
+		var current = document.getElementsByClassName("current");
+		current[0].className = current[0].className.replace(" current", "");
+		this.className += " current";
+	});
+}
+
+//Accordion
+var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			this.classList.toggle("current-bar");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			} 
+		});
+	}
+
+//MagnificPopup Video
+$('#play-video').magnificPopup({
+	disableOn: 300,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 300,
+	preloader: false,
+	fixedContentPos: false
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
