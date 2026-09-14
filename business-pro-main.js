e6f61b7bf2e0eb3536c54c76c7003bb74f1a64e2
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-13 21:36:06 -0500

    add files

---
+++ business-pro-main.js
@@ -0,0 +1,104 @@
+"use strict";
+//Slick Slider (1) One
+$(document).ready(function(){
+	$('.feature_slider').slick({
+		slidesToShow: 4,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 801,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+					fade: true
+				}			
+			},
+		]
+	});
+});
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.client-slider').slick({
+		dots: true,
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		arrows: false,
+		autoplay: false,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint:993,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				}
+						
+			},
+		]
+	});
+});
+
+//Animate Scroll
+$( ".services a" ).click(function() {
+	$('.business_area_one').animatescroll();
+});
+
+$( ".benefits a" ).click(function() {
+	$('.business_area_three').animatescroll();
+});
+
+$( ".portfolio a" ).click(function() {
+	$('.business_area_five').animatescroll();
+});
+
+$( ".news a" ).click(function() {
+	$('.business_area_eight').animatescroll();
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
