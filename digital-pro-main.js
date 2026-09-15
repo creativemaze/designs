0d5998c7ddae7b65521dde53f1ea8d47cf7c077f
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-14 21:44:26 -0500

    add files

---
+++ digital-pro-main.js
@@ -0,0 +1,102 @@
+"use strict";
+//Slick Slider
+$( window ).on( "load", function(){
+	$('.digital_slider').slick({
+		dots: false,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		fade: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,		
+	});
+	
+	$('.digital_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$('.title_head a.active').removeClass('active');
+			$('.title_head a').eq(nextSlide).addClass('active');
+	});
+  
+	$('.title_head a').on('click', function(e){
+		e.preventDefault();
+		$('.title_head a.active').removeClass('active');
+		$(this).addClass('active');
+		var targetSlide = $(this).data('target');
+		$('.digital_slider').slick('slickGoTo', targetSlide );
+	});
+	
+});
+
+//Animate Scroll
+$( ".features a" ).click(function() {
+	$('.digital_area_one').animatescroll();
+});
+
+$( ".services a" ).click(function() {
+	$('.digital_area_two').animatescroll();
+});
+
+$( ".process a" ).click(function() {
+	$('.digital_area_three').animatescroll();
+});
+
+$( ".pricing a" ).click(function() {
+	$('.digital_area_five').animatescroll();
+});
+
+$( ".charts a" ).click(function() {
+	$('.digital_area_six').animatescroll();
+});
+
+$( ".news a" ).click(function() {
+	$('.digital_area_eight').animatescroll();
+});
+
+//MagnificPopup Video
+$('#play-video').magnificPopup({
+	disableOn: 300,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 300,
+	preloader: false,
+	fixedContentPos: false
+});
+
+//Counter script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
