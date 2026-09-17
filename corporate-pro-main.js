0dfc452ffd8f50dfe3dfaedeb5dd5ae7b1d7ad02
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-17 10:48:48 -0500

    add files

---
+++ corporate-pro-main.js
@@ -0,0 +1,96 @@
+"use strict";
+//Slick Slider (1) One
+$( window ).on( "load", function(){
+	$('.main_slider').slick({
+		autoplay: true,
+		infinite: true,
+		autoplaySpeed: 5000,
+		speed: 1000,
+		fade: true,
+		prevArrow: '.prev_nav',
+		nextArrow: '.next_nav',
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		dots: false,
+	});
+});
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.client-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: true,
+		autoplay: false,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+	});
+});
+
+//Animate Scroll
+$( ".about a" ).click(function() {
+	$('.corporate_area_one').animatescroll();
+});
+
+$( ".services a" ).click(function() {
+	$('.corporate_area_two').animatescroll();
+});
+
+$( ".strategies a" ).click(function() {
+	$('.corporate_area_three').animatescroll();
+});
+
+$( ".outlook a" ).click(function() {
+	$('.corporate_area_five').animatescroll();
+});
+
+$( ".reviews a" ).click(function() {
+	$('.corporate_area_six').animatescroll();
+});
+
+$( ".news a" ).click(function() {
+	$('.corporate_area_seven').animatescroll();
+});
+
+//MagnificPopup Video
+$('#play-video').magnificPopup({
+	disableOn: 300,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 300,
+	preloader: false,
+	fixedContentPos: false
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
