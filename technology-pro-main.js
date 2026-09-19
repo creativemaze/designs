743310d14e95da779f552486de5b2c5604905aa1
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-19 16:56:17 -0500

    add files

---
+++ technology-pro-main.js
@@ -0,0 +1,102 @@
+"use strict";
+//Slick Slider
+$(document).ready(function(){
+	$('.bg_slider').slick({
+		dots: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: false,
+		fade: true,
+		adaptiveHeight: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,		
+	});
+	
+	$('.bg_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$('.bg_info > div.active').removeClass('active');
+			$('.bg_info > div').eq(nextSlide).addClass('active');
+	});
+  
+	$('.bg_info > div').on('mouseover', function(e){
+		e.preventDefault();
+		$('.bg_info > div.active').removeClass('active');
+		$(this).addClass('active');
+		var targetSlide = $(this).data('target');
+		$('.bg_slider').slick('slickGoTo', targetSlide );
+	});
+	
+});
+
+//Animate Scroll
+$( ".products a" ).click(function() {
+	$('.tech_area_one').animatescroll();
+});
+
+$( ".services a" ).click(function() {
+	$('.tech_area_two').animatescroll();
+});
+
+$( ".solutions a" ).click(function() {
+	$('.tech_area_three').animatescroll();
+});
+
+$( ".features a" ).click(function() {
+	$('.tech_area_four').animatescroll();
+});
+
+$( ".faqs a" ).click(function() {
+	$('.tech_area_six').animatescroll();
+});
+
+$( ".news a" ).click(function() {
+	$('.tech_area_seven').animatescroll();
+});
+
+//F.A.Q.S Accordion
+var acc = document.getElementsByClassName("accordion");
+	var i;
+
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			this.classList.toggle("active-bar");
+			var panel = this.nextElementSibling;
+			if (panel.style.maxHeight) {
+				panel.style.maxHeight = null;
+			} else {
+				panel.style.maxHeight = panel.scrollHeight + "px";
+			} 
+		});
+	}
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
