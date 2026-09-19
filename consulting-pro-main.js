75eb2cbf60d94e22e04b38b2870a594112d93cad
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-18 19:01:27 -0500

    add files

---
+++ consulting-pro-main.js
@@ -0,0 +1,128 @@
+"use strict";
+//Slick Slider (1) One
+$(document).ready(function(){
+	$('.border_slider').slick({
+		slidesToShow: 3,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 601,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+					fade: true
+				}			
+			},
+		]
+	});
+});
+
+//Slick Slider (2) Two
+$(document).ready(function(){
+	$('.bg_slider').slick({
+		dots: false,
+		autoplaySpeed: 5000,
+		slidesToShow: 1,
+		fade: true,
+		slidesToScroll: 1,
+		prevArrow: '.prev_nav',
+		nextArrow: '.next_nav',
+		autoplay: true,
+		speed: 1000,
+		
+	});
+});
+
+//Slick Slider (3) Three
+$(document).ready(function(){
+	$('.feature_slider').slick({
+		slidesToShow: 3,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+					fade: true
+				}			
+			},
+		]
+	});
+});
+
+//Animate Scroll
+$( ".about a" ).click(function() {
+	$('.consult_area_two').animatescroll();
+});
+
+$( ".case_studies a" ).click(function() {
+	$('.consult_area_three').animatescroll();
+});
+
+$( ".process a" ).click(function() {
+	$('.consult_area_four').animatescroll();
+});
+
+$( ".news a" ).click(function() {
+	$('.consult_area_five').animatescroll();
+});
+
+$( ".testimonials a" ).click(function() {
+	$('.consult_area_seven').animatescroll();
+});
+
+$( ".success a" ).click(function() {
+	$('.consult_area_eight').animatescroll();
+});
+
+//Counter Script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
