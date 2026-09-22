735a333a35b0e71d962623f1a60250d21fd75e56
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-22 18:49:25 -0500

    add files

---
+++ travel-pro-main.js
@@ -0,0 +1,109 @@
+"use strict";
+//Slick Slider
+$(document).ready(function(){
+	$('.tour_slider').slick({
+		dots: false,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		autoplay: false,
+		fade: true,
+		adaptiveHeight: true,
+		speed: 1000,
+		pauseOnHover: false,
+		pauseOnFocus: false,		
+	});
+	
+	$('.tour_slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+		$('.tour_info > div.active').removeClass('active');
+			$('.tour_info > div').eq(nextSlide).addClass('active');
+	});
+  
+	$('.tour_info > div').on('mouseover', function(e){
+		e.preventDefault();
+		$('.tour_info > div.active').removeClass('active');
+		$(this).addClass('active');
+		var targetSlide = $(this).data('target');
+		$('.tour_slider').slick('slickGoTo', targetSlide );
+	});
+	
+});
+
+//Animate Scroll
+$( ".destinations a" ).click(function() {
+	$('.travel_area_one').animatescroll();
+});
+
+$( ".tours a" ).click(function() {
+	$('.travel_area_three').animatescroll();
+});
+
+$( ".features a" ).click(function() {
+	$('.travel_area_four').animatescroll();
+});
+
+$( ".highlights a" ).click(function() {
+	$('.travel_area_six').animatescroll();
+});
+
+$( ".blog a" ).click(function() {
+	$('.travel_area_seven').animatescroll();
+});
+
+//MagnificPopup Video
+$('#play-video').magnificPopup({
+	disableOn: 300,
+	type: 'iframe',
+	mainClass: 'mfp-fade',
+	removalDelay: 300,
+	preloader: false,
+	fixedContentPos: false
+});
+
+//MagnificPopup Gallery
+$('.bg_image').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery:{
+		enabled: true
+	},
+});
+
+$('.footer_grid').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery: {
+		enabled: true
+	},
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
