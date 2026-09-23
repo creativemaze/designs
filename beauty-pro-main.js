e125f5d32deacf9ec3c91f86fe3200c3748e3aa7
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-23 12:27:38 -0500

    add files

---
+++ beauty-pro-main.js
@@ -0,0 +1,138 @@
+"use strict";
+///Slick Slider (1) One
+$( window ).on( "load", function(){
+	$('.header_slider').slick({
+		autoplay: true,
+		infinite: true,
+		autoplaySpeed: 6000,
+		speed: 1000,
+		fade: true,
+		prevArrow: '.prev_button',
+		nextArrow: '.next_button',
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		dots: false,
+		swipe: false
+	});
+	
+});
+
+//Slick Slider (2) Two
+$( window ).on( "load", function(){
+	$('.area_slider').slick({
+		slidesToShow: 4,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 1025,
+				settings: 
+					{
+						slidesToShow: 1,
+						dots: true,
+						fade: true
+					}
+						
+			},
+			
+		]
+	});
+});
+
+//Slick Slider (3) Three
+$(document).ready(function(){
+	$('.border_slider').slick({
+		slidesToShow: 3,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 601,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+					fade: true
+				}			
+			},
+		]
+	});
+});
+
+//Animate Scroll
+$( ".features a" ).click(function() {
+	$('.spa_area_three').animatescroll();
+});
+
+$( ".about a" ).click(function() {
+	$('.spa_area_four').animatescroll();
+});
+
+$( ".treatments a" ).click(function() {
+	$('.spa_area_five').animatescroll();
+});
+
+$( ".specials a" ).click(function() {
+	$('.spa_area_six').animatescroll();
+});
+
+$( ".articles a" ).click(function() {
+	$('.spa_area_nine').animatescroll();
+});
+
+//Counter script
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//MagnificPopup Gallery
+$('.footer_grid').magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery: {
+		enabled: true
+	},
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
