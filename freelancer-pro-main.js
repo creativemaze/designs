2ae831eaef0b147181a8f9c1597d03d6a0115ad4
creativemaze <61708713+creativemaze@users.noreply.github.com>
2026-09-25 10:46:13 -0500

    add files

---
+++ freelancer-pro-main.js
@@ -0,0 +1,128 @@
+"use strict";
+//Slick Slider
+$( window ).on( "load", function(){
+	$('.feature_slider').slick({
+		slidesToShow: 3,
+		autoplay: false,
+		infinite: true,
+		speed: 1000,
+		arrows: false,
+		dots: false,
+		pauseOnHover: false,
+		pauseOnFocus: false,
+		responsive: [
+			{
+				breakpoint: 993,
+				settings: {
+					slidesToShow: 1,
+					dots: true,
+					fade: true
+				}			
+			},
+		]
+	});
+});
+
+//Animate Scroll
+$( ".head_btn a" ).click(function() {
+	$('.freelance_area_eight').animatescroll();
+});
+
+$( ".about a, .right_btn" ).click(function() {
+	$('.freelance_area_two').animatescroll();
+});
+
+$( ".services a" ).click(function() {
+	$('.freelance_area_three').animatescroll();
+});
+
+$( ".skills a" ).click(function() {
+	$('.freelance_area_four').animatescroll();
+});
+
+$( ".portfolio a, .left_btn" ).click(function() {
+	$('.freelance_area_five').animatescroll();
+});
+
+$( ".blog a" ).click(function() {
+	$('.freelance_area_six').animatescroll();
+});
+
+
+jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 2000
+    });
+});
+
+//Animated Progress Bar
+$(document).ready(function($) {
+  function animateElements() {
+	$('.progressbar').each(function() {
+		var elementPos = $(this).offset().top;
+		var topOfWindow = $(window).scrollTop();
+		var percent = $(this).find('.circle').attr('data-percent');
+		var percentage = parseInt(percent, 10) / parseInt(100, 10);
+		var animate = $(this).data('animate');
+		if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+			$(this).data('animate', true);
+			$(this).find('.circle').circleProgress({
+			startAngle: -Math.PI / 2,
+			value: percent / 100,
+			thickness: 14,
+			size: 125,
+			animation: {
+				duration: 2800,
+			},
+			
+			fill: {
+				color: '#d31027'
+			},
+			
+			emptyFill:{
+				color: '#111111'
+			}
+			
+			}).on('circle-animation-progress', function(event, progress, stepValue) {
+				$(this).find('div').text((stepValue * 100).toFixed(1) + "%");
+			}).stop();
+		}
+    });
+}
+
+//Show animated elements
+animateElements();
+	$(window).scroll(animateElements);
+});
+
+//Animate on Scroll default settings
+$(window).on('load', function () {
+	AOS.refresh();
+});
+
+$(function () {
+	AOS.init({
+		// Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+		offset: 200, // offset (in px) from the original trigger point
+		delay: 300, // values from 0 to 3000, with step 50ms
+		duration: 1000, // values from 0 to 3000, with step 50ms
+		easing: 'ease', // default easing for AOS animations
+		once: true, // whether animation should happen only once - while scrolling down
+		mirror: true, // whether elements should animate out while scrolling past them
+		anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+	});
+});	
+
+//Page Preloader
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loading").style.visibility = "visible";
+		 
+    } else {
+        document.querySelector("#loading").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
